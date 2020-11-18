a84e748206c0c7d1f82eda944be25f1a405e2783
Jan Rudolf <jan.rudolf@plus4u.net>
2020-11-18 11:47:16 +0100

    Meta model now correctly merges profiles for appType entitites.

--- uu_devkit_compareg01/src/core/configs/metamodel-json.js
+++ uu_devkit_compareg01/src/core/configs/metamodel-json.js
@@ -4,38 +4,80 @@ const ServerRoot = require("./server-root");
 
 const PossibleFilePaths = [
   ["app", "config", "metamodel"],
-  ["app", "env", "metamodel"],
+  ["env", "metamodel"],
 ];
 
-const MetamodelJson = {
-  load() {
+function processUcProfileMap(ucMap, appType, appCode, mmdProfiles) {
+  Object.entries(appType.useCaseProfileMap).forEach(([fullUc, profileStrings]) => {
+    if (!fullUc.includes(appCode)) return;
+
+    const uc = fullUc.replace(appCode + "/", "");
+    const ucProfiles = [];
+    let profileString = profileStrings;
+    if (Array.isArray(profileStrings)) {
+      profileString = profileStrings[0];
+    }
+    profileString.split("").forEach((char, i) => char === "1" && ucProfiles.push(mmdProfiles[i]));
+    ucMap[uc] = ucProfiles;
+  });
+  return ucMap;
+}
+
+class MetamodelJson {
+  constructor() {
+    this._mmdFldPath = null;
+    this._mmd = null;
+    this._appTypes = null;
+  }
+
+  loadMmdPath() {
+    if (this._mmdFldPath) return this._mmdFldPath;
     for (let mmdPath of PossibleFilePaths) {
       let metamodelPath = path.join(ServerRoot.root, ...mmdPath);
       if (fs.existsSync(metamodelPath)) {
-        const mmdFiles = fs.readdirSync(metamodelPath);
-        let awscFile = mmdFiles.find((file) => !file.startsWith("type-"));
-        return JSON.parse(fs.readFileSync(path.join(metamodelPath, awscFile)));
+        this._mmdFldPath = metamodelPath;
+        return this._mmdFldPath;
       }
     }
-  },
+  }
+
+  load() {
+    if (this._mmd) return this._mmd;
+    let mmdFldPath = this.loadMmdPath();
+    if (mmdFldPath) {
+      const mmdFiles = fs.readdirSync(mmdFldPath);
+      this._appTypes = [];
+      mmdFiles.forEach((file) => {
+        let parsedFile = JSON.parse(fs.readFileSync(path.join(mmdFldPath, file)));
+        if (!file.startsWith("type-")) {
+          this._mmd = parsedFile;
+        } else {
+          this._appTypes.push(parsedFile);
+        }
+      });
+      return this._mmd;
+    }
+  }
+
+  loadAppTypes() {
+    if (this._appTypes) return this._appTypes;
+    this.load();
+    return this._appTypes;
+  }
 
   getUcProfileMap() {
     let mmdJson = this.load();
     if (!mmdJson) return;
 
+    let ucMap = {};
     const appCode = mmdJson.code;
     const mmdProfiles = mmdJson.profileList.map((prof) => prof.code);
-    let ucMap = {};
-    Object.entries(mmdJson.useCaseProfileMap).forEach(([fullUc, profileString]) => {
-      if (!fullUc.includes(appCode)) return;
-
-      const uc = fullUc.replace(appCode + "/", "");
-      const ucProfiles = [];
-      profileString.split("").forEach((char, i) => char === "1" && ucProfiles.push(mmdProfiles[i]));
-      ucMap[uc] = ucProfiles;
+    ucMap = processUcProfileMap(ucMap, mmdJson, appCode, mmdProfiles);
+    this.loadAppTypes().forEach((appType) => {
+      ucMap = processUcProfileMap(ucMap, appType, appCode, mmdProfiles);
     });
     return ucMap;
-  },
-};
+  }
+}
 
-module.exports = MetamodelJson;
+module.exports = new MetamodelJson();
